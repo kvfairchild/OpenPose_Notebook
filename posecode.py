2237970ed9bf80e8b8ba66af556dcd205d133814
Katherine Gallagher <kvg0@mit.edu>
2019-04-01 10:44:02 -0400

    dl youtube

--- posecode.py
+++ posecode.py
@@ -23,7 +23,7 @@ def is_valid_youtube(YOUTUBE_LINK):
 
 def install_openpose():
 
-	from os.path import exists
+	from os.path import splitext, exists
 
 	git_repo_url = 'https://github.com/CMU-Perceptual-Computing-Lab/openpose.git'
 	project_name = splitext(basename(git_repo_url))[0]
