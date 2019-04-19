0874714a3f975592692ec451a24b96745c8b1643
Katherine Gallagher <kvgallagher@Katherines-MacBook-Pro.local>
2019-04-19 15:42:54 -0400

    add scripts

---
+++ run_openpose.sh
@@ -0,0 +1,10 @@
+rm -rf clip.mp4
+youtube-dl -f 'bestvideo[ext=mp4]' --output 'clip.mp4' $YOUTUBE_LINK
+
+# cut the first 5 seconds
+ffmpeg -y -loglevel info -i clip.mp4 -t $CLIP_LEN_S video.mp4
+# detect poses on the these 5 seconds
+rm openpose.avi
+cd openpose && ./build/examples/openpose/openpose.bin --video ../video.mp4 --write_json ./output/ --display 0  --write_video ../openpose.avi
+# convert the result into MP4
+ffmpeg -y -loglevel info -i openpose.avi output.mp4
