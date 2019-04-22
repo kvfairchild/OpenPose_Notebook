340b90852d7773cee409e610cf0bb418bff87158
Katherine Gallagher <kvgallagher@Katherines-MacBook-Pro.local>
2019-04-22 18:47:02 -0400

    update run_openpose

--- scripts/run_openpose.sh
+++ scripts/run_openpose.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
-YOUTUBE_LINK=$1
+VIDEO_PATH=$1
 CLIP_START=$2
 CLIP_END=$3
 
 rm -rf clip.mp4
-youtube-dl -f 'bestvideo[ext=mp4]' --output 'clip.mp4' $YOUTUBE_LINK
+
+if [ -f $VIDEO_PATH ]
+then
+	echo $VIDEO_PATH ' is a file'
+	mv $VIDEO_PATH 'clip.mp4'
+	.
+else
+	echo $VIDEO_PATH 'is not a file'
+	youtube-dl -f 'bestvideo[ext=mp4]' --output 'clip.mp4' $VIDEO_PATH
+	.
+fi
 
 # cut the first n seconds
-ffmpeg -y -loglevel info -i clip.mp4 -ss $CLIP_START -to $CLIP_END -async 1 -strict -2 video.mp4
+ffmpeg -y -loglevel info -i 'clip.mp4' -ss $CLIP_START -to $CLIP_END -async 1 -strict -2 video.mp4
 # detect poses on the these n seconds
 rm openpose.avi
 cd openpose && ./build/examples/openpose/openpose.bin --video ../video.mp4 --write_json ./output/ --display 0  --write_video ../openpose.avi
