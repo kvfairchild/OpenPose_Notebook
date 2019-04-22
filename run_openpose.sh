00ddede10071d9d7c33fc6dbf0310ce4b55b5096
Katherine Gallagher <kvgallagher@Katherines-MacBook-Pro.local>
2019-04-22 11:02:17 -0400

    update run_openpose

--- run_openpose.sh
+++ run_openpose.sh
@@ -2,16 +2,13 @@
 YOUTUBE_LINK=$1
 CLIP_LEN_S=$2
 
-echo $YOUTUBE_LINK
-echo $CLIP_LEN_S
-
 rm -rf clip.mp4
 youtube-dl -f 'bestvideo[ext=mp4]' --output 'clip.mp4' $YOUTUBE_LINK
 
-# cut the first 5 seconds
+# cut the first n seconds
 ffmpeg -y -loglevel info -i clip.mp4 -t $CLIP_LEN_S video.mp4
-# detect poses on the these 5 seconds
+# detect poses on the these n seconds
 rm openpose.avi
 cd openpose && ./build/examples/openpose/openpose.bin --video ../video.mp4 --write_json ./output/ --display 0  --write_video ../openpose.avi
 # convert the result into MP4
-ffmpeg -y -loglevel info -i openpose.avi output.mp4
+ffmpeg -y -loglevel info -i ../openpose.avi output.mp4
