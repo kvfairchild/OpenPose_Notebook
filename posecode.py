1de6aa8cf76199af2e8466548ff6a8fe0d0e6b2c
Katherine Gallagher <kvgallagher@Katherines-MacBook-Pro.local>
2019-04-22 17:19:27 -0400

    update display_video

--- posecode.py
+++ posecode.py
@@ -13,35 +13,18 @@ def is_valid_youtube(YOUTUBE_LINK):
     if r.status_code == 200:
       return True
     else:
-      print("Code: " + f'{r.status_code}' + "\n")
-      print("Please enter a valid YouTube URL or upload a video below.")
       return False
     
   except requests.ConnectionError:
-    print("Failed to connect.")
+    print("Failed to connect to YouTube.")
     return False
-
-
-def openpose_installed():
-
-	import os
-	from os.path import splitext, basename, exists
-
-	git_repo_url = 'https://github.com/CMU-Perceptual-Computing-Lab/openpose.git'
-	project_name = splitext(basename(git_repo_url))[0]
-
-	if exists(project_name):
-		print("Existing OpenPose installation detected, moving on.")
-		return True
-	else:
-		return False
 
 
 def display_video(video_path, youtube):
 
-	from IPython.display import YouTubeVideo, HTML
-
 	if youtube:
+
+		from IPython.display import YouTubeVideo, HTML
 
 		# display video
 		YOUTUBE_ID = _get_id_from_link(video_path)
@@ -51,7 +34,20 @@ def display_video(video_path, youtube):
 
 	else:
 
-		return display_local(video_path)
+		display_local(video_path)
+
+
+def display_local(video_path, width=640, height=480):
+ 
+  import io
+  import base64
+  from IPython.display import HTML
+ 
+  video_encoded = base64.b64encode(io.open(video_path, 'rb').read())
+ 
+  return HTML(data='''<video width="{0}" height="{1}" alt="test" controls>
+                        <source src="data:video/mp4;base64,{2}" type="video/mp4" />
+                      </video>'''.format(width, height, video_encoded.decode('ascii')))
 
 
 def run_openpose(video):
@@ -68,16 +64,6 @@ def run_openpose(video):
 	os.system("!cd openpose && ./build/examples/openpose/openpose.bin --video ../video.mp4 --write_json ./output/ --display 0  --write_video ../openpose.avi")
 	# convert the result into MP4
 	os.system("!ffmpeg -y -loglevel info -i openpose.avi output.mp4")
-
-
-def display_local(file_name, width=640, height=480):
-  import io
-  import base64
-  from IPython.display import HTML
-  video_encoded = base64.b64encode(io.open(file_name, 'rb').read())
-  return HTML(data='''<video width="{0}" height="{1}" alt="test" controls>
-                        <source src="data:video/mp4;base64,{2}" type="video/mp4" />
-                      </video>'''.format(width, height, video_encoded.decode('ascii')))
 
 
 
