dd3b6e5fc8bc320b3693aa24db5045f995294979
Katherine Gallagher <kvgallagher@Katherines-MacBook-Pro.local>
2019-04-22 18:51:28 -0400

    update run_openpose

--- scripts/run_openpose.sh
+++ scripts/run_openpose.sh
@@ -7,20 +7,16 @@ rm -rf clip.mp4
 
 if [ -f $VIDEO_PATH ]
 then
-	echo $VIDEO_PATH ' is a file'
 	mv $VIDEO_PATH 'clip.mp4'
-	.
 else
-	echo $VIDEO_PATH 'is not a file'
 	youtube-dl -f 'bestvideo[ext=mp4]' --output 'clip.mp4' $VIDEO_PATH
-	.
 fi
 
 # cut the first n seconds
 ffmpeg -y -loglevel info -i 'clip.mp4' -ss $CLIP_START -to $CLIP_END -async 1 -strict -2 video.mp4
 # detect poses on the these n seconds
 rm openpose.avi
-cd openpose && ./build/examples/openpose/openpose.bin --video ../video.mp4 --write_json ./output/ --display 0  --write_video ../openpose.avi
+cd openpose && ./build/examples/openpose/openpose.bin --net_resolution '1312x736' --scale_number 4 --scale_gap 0.25  --hand --hand_scale_number 6 --hand_scale_range 0.4 --face --video ../video.mp4 --write_json ./output/ --display 0  --write_video ../openpose.avi
 # convert the result into MP4
 ffmpeg -y -loglevel info -i ../openpose.avi output.mp4
 
